bb908761abab851245d7780267e7afcc38377174
SK-CSE <saurabh.cse04@gmail.com>
2017-04-02 16:18:31 +0530

    updated for graph

---
+++ graph/graphWithoutPrototype.js
@@ -0,0 +1,36 @@
+function Graph(v) {
+   this.vertices = v;
+   this.edges = 0;
+   this.adj = [];
+   for (var i = 0; i < this.vertices; i++) {
+      this.adj[i] = [];
+      this.adj[i].push("");
+   }
+   this.addEdge = addEdge;
+   this.showGraph = showGraph;
+}
+
+function addEdge(v,w) {
+   this.adj[v].push(w);
+   this.adj[w].push(v);
+   this.edges++;
+}
+
+function showGraph() {
+   for (var i = 0; i < this.vertices; ++i) {
+      console.log(i + " -> ");
+      for (var j = 0; j < this.vertices; ++j) {
+         if (this.adj[i][j] != undefined)
+            console.log(this.adj[i][j] + ' ');
+      }
+   }
+}
+
+g = new Graph(6);
+g.addEdge(0,5);
+g.addEdge(0,3);
+g.addEdge(5,2);
+g.addEdge(3,4);
+g.addEdge(2,1);
+
+g.showGraph();

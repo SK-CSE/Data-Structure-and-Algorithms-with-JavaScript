3203fd6589076dc75ae1e4f847ccfcfd722907ae
Saurabh Kumar <saurabh.cse04@gmail.com>
2020-01-17 11:11:18 +0530

    👅

---
+++ Strings/firstNonRepeatedCharacter.js
@@ -0,0 +1,12 @@
+function firstNonRepeatedCharacter(string) {
+    for (var i = 0; i < string.length; i++) {
+        var c = string.charAt(i);
+        if (string.indexOf(c) == i && string.indexOf(c, i + 1) == -1) {
+            return c;
+        }
+    }
+    return null;
+}
+
+var someString = 'asdad';
+console.log(firstNonRepeatedCharacter(someString));
